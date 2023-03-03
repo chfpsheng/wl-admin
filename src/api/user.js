d0d5d815054a5b890c25a94114070d8a797998d3
yangchufa591 <yangchufa591@pingan.com>
2023-03-03 16:49:35 +0800

    1.本地调整成mock模式，如果需要调整成实际模式，需要修改vue.config.js文件中before配置项，以及VUE_APP_BASE_API

--- src/api/user.js
+++ src/api/user.js
@@ -1,41 +1,41 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 export function login(data) {
-  console.log('new')
+  console.log("new");
   return request({
-    url: '/cloud-user/login/login',
-    method: 'post',
+    url: "/vue-element-admin/user/login",
+    method: "post",
     data
-  })
+  });
 }
 
 export function getInfo(token) {
   return request({
-    url: '/vue-element-admin/user/info',
-    method: 'get',
+    url: "/vue-element-admin/user/info",
+    method: "get",
     params: { token }
-  })
+  });
 }
 
 export function logout() {
   return request({
-    url: '/cloud-user/login/logout',
-    method: 'post'
-  })
+    url: "/cloud-user/login/logout",
+    method: "post"
+  });
 }
 
 export function getUser(params) {
   return request({
-    url: '/cloud-user/sysRole/queryByUserId',
-    method: 'post',
+    url: "/cloud-user/sysRole/queryByUserId",
+    method: "post",
     data: params
-  })
+  });
 }
 
 // 修改密码
 export function changePassWord(params) {
   return request({
     url: `/cloud-user/sysUser/changePassWord?oldPassword=${params.oldPassword}&newPassword=${params.newPassword}&userId=${params.userId}`,
-    method: 'post'
-  })
+    method: "post"
+  });
 }
--- vue.config.js
+++ vue.config.js
@@ -1,50 +1,50 @@
-'use strict'
-const path = require('path')
-const defaultSettings = require('./src/settings.js')
+"use strict";
+const path = require("path");
+const defaultSettings = require("./src/settings.js");
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
 function getProxyConfig() {
-  const config = {}
+  const config = {};
   for (const key in process.env) {
     if (Object.hasOwnProperty.call(process.env, key)) {
-      const envProxyPrefix = 'PROXY_CONFIG'
+      const envProxyPrefix = "PROXY_CONFIG";
       if (key.startsWith(envProxyPrefix)) {
-        const item = process.env[key]
-        const splitStr = '-->>'
-        const itemConfig = item.split(splitStr)
-        const prefix = itemConfig[0].trim()
-        const target = itemConfig[1].trim()
-        const pathRewrite = prefix.replace(/\/\S+\//g, '/')
+        const item = process.env[key];
+        const splitStr = "-->>";
+        const itemConfig = item.split(splitStr);
+        const prefix = itemConfig[0].trim();
+        const target = itemConfig[1].trim();
+        const pathRewrite = prefix.replace(/\/\S+\//g, "/");
         const proxyConfig = {
           target,
           changeOrigin: true,
           pathRewrite: {
             [prefix]: pathRewrite
           }
-        }
-        config[prefix] = proxyConfig
+        };
+        config[prefix] = proxyConfig;
       }
     }
   }
-  console.log(config)
-  return config
+  console.log(config);
+  return config;
 }
 
-const name = defaultSettings.title || '互联网医院管理系统' // page title
+const name = defaultSettings.title || "互联网医院管理系统"; // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
 // For example, Mac: sudo npm run
 // You can change the port by the following method:
 // port = 9527 npm run dev OR npm run dev --port = 9527
-const port = process.env.port || process.env.npm_config_port || 9527 // dev port
+const port = process.env.port || process.env.npm_config_port || 9527; // dev port
 // 导入compression-webpack-plugin
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 // 定义压缩文件类型
-const productionGzipExtensions = ['js', 'css']
+const productionGzipExtensions = ["js", "css"];
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -55,20 +55,20 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: './',
-  outputDir: 'dist',
-  assetsDir: 'static',
+  publicPath: "./",
+  outputDir: "dist",
+  assetsDir: "static",
   lintOnSave: false,
   // lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   css: {
-    extract: false,  // false热加载
+    extract: false, // false热加载
     sourceMap: false,
     loaderOptions: {
       stylus: {
-        'resolve url': true,
+        "resolve url": true,
         // 自定义主题场景
-        import: [path.resolve(__dirname, './src/assets/css/base.styl')]
+        import: [path.resolve(__dirname, "./src/assets/css/base.styl")]
       }
     }
   },
@@ -82,12 +82,12 @@ module.exports = {
     },
     proxy: {
       ...getProxyConfig(), // 自定义本地开发联调转发
-      '/cloud-*': {
+      "/cloud-*": {
         target: process.env.VUE_APP_BASE_API_PROXY, // 默认转发
         changeOrigin: true
       }
-    }
-    // before: require('./mock/mock-server.js')
+    },
+    before: require("./mock/mock-server.js")
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
@@ -95,14 +95,14 @@ module.exports = {
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src')
+        "@": resolve("src")
       }
     },
     plugins: [
       new CompressionWebpackPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        filename: "[path].gz[query]",
+        algorithm: "gzip",
+        test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
         threshold: 10240,
         minRatio: 0.8
       })
@@ -111,72 +111,72 @@ module.exports = {
   chainWebpack(config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
     // it can improve the speed of the first screen, it is recommended to turn on preload
-    config.plugin('preload').tap(() => [{
-      rel: 'preload',
-      // to ignore runtime.js
-      // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-      fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-      include: 'initial'
-    }])
+    config.plugin("preload").tap(() => [
+      {
+        rel: "preload",
+        // to ignore runtime.js
+        // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+        include: "initial"
+      }
+    ]);
 
     // when there are many pages, it will cause too many meaningless requests
-    config.plugins.delete('prefetch')
+    config.plugins.delete("prefetch");
 
     // set svg-sprite-loader
     config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
+      .rule("svg")
+      .exclude.add(resolve("src/icons"))
+      .end();
     config.module
-      .rule('icons')
+      .rule("icons")
       .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
+      .include.add(resolve("src/icons"))
       .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
       .options({
-        symbolId: 'icon-[name]'
+        symbolId: "icon-[name]"
       })
-      .end()
+      .end();
 
-    config
-      .when(process.env.NODE_ENV !== 'development',
-        config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-              // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
-          config
-            .optimization.splitChunks({
-              chunks: 'all',
-              cacheGroups: {
-                libs: {
-                  name: 'chunk-libs',
-                  test: /[\\/]node_modules[\\/]/,
-                  priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
-                },
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                },
-                commons: {
-                  name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
-                  minChunks: 3, //  minimum common number
-                  priority: 5,
-                  reuseExistingChunk: true
-                }
-              }
-            })
-          // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
-          config.optimization.runtimeChunk('single')
+    config.when(process.env.NODE_ENV !== "development", config => {
+      config
+        .plugin("ScriptExtHtmlWebpackPlugin")
+        .after("html")
+        .use("script-ext-html-webpack-plugin", [
+          {
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+            inline: /runtime\..*\.js$/
+          }
+        ])
+        .end();
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial" // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: "chunk-elementUI", // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
+          }
         }
-      )
+      });
+      // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+      config.optimization.runtimeChunk("single");
+    });
   }
-}
+};
